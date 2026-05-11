7a54adf9722b9bc055c18bfb0ca5f4f80213421e
Girish Limaye <girishlimaye@Girishs-Mac-mini.local>
2026-05-10 20:07:38 -0700

    Support lowercase ai-pulse API path

--- api/responses.js
+++ api/responses.js
@@ -8,6 +8,7 @@ function setCors(res) {
   res.setHeader("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN || "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Cache-Control", "no-store");
 }
 
 function cleanText(value, maxLength = 1200) {
